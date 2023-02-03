228faa6f35f504664ecf0b9510523a75c6fd08d3
Obiri Job <obirijob20@gmail.com>
2023-02-03 14:13:12 +0300

    Pagination and offset

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,18 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRocket } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
-function Grid({ capsules, openCapsuleModal }) {
+function Grid({ capsules, openCapsuleModal, nextPage, prevPage }) {
   return (
-    <div className="grid">
-      {capsules.map(capsule => (
-        <div className="capsule" onClick={() => openCapsuleModal(capsule)}>
-          <FontAwesomeIcon icon={faRocket} size="2x" />
-          <span>
-            {capsule.capsule_serial} - {capsule.type}
-          </span>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="grid">
+        {capsules.map(capsule => (
+          <div className="capsule" onClick={() => openCapsuleModal(capsule)}>
+            <FontAwesomeIcon icon={faRocket} size="2x" />
+            <span>
+              {capsule.capsule_serial} - {capsule.type}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className="pager">
+        <button onClick={prevPage} className="prev">
+          Previous Page
+        </button>
+        <button onClick={nextPage} className="nextPage">
+          Next Page
+        </button>
+      </div>
+    </>
   )
 }
 
