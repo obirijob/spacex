a212ce350e2cea0deb557590992c4e39e04ceed3
Obiri Job <obirijob20@gmail.com>
2023-02-03 13:53:29 +0300

    Modal

--- src/components/ModalCapsule.jsx
+++ src/components/ModalCapsule.jsx
@@ -3,6 +3,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRocket } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
+import moment from 'moment'
 
 function ModalCapsule({ capsule, close }) {
   const {
@@ -19,6 +20,61 @@ function ModalCapsule({ capsule, close }) {
   return (
     <div className="modal">
       <FontAwesomeIcon icon={faRocket} />
+      <div className="title">{capsule_serial}</div>
+      <p>{details}</p>
+      <p>
+        <ul>
+          <li>
+            Capsule Id: <span>{capsule_id}</span>
+          </li>
+          <li>
+            Status: <span>{status}</span>
+          </li>
+          <li>
+            Original Launch:{' '}
+            <span>
+              {moment(original_launch).format('D dd, MMM YYYY hh:mm:ss a')}
+            </span>
+          </li>
+          <li>
+            Landings: <span>{landings}</span>
+          </li>
+          <li>
+            Type: <span>{type}</span>
+          </li>
+          <li>
+            Reuse Count: <span>{reuse_count}</span>
+          </li>
+        </ul>
+      </p>
+      {missions.length > 0 ? (
+        <>
+          <div className="title">
+            Missions <span className="badge">{missions.length}</span>{' '}
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Flight</th>
+              </tr>
+            </thead>
+            <tbody>
+              {missions.map(({ name, flight }) => (
+                <tr>
+                  <td>{name}</td>
+                  <td>{flight}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <div className="no-mission" style={{ margin: 10 }}>
+          No Missions
+        </div>
+      )}
       <button onClick={close}>Close Modal</button>
     </div>
   )
